31b0ad7b38cca8a7666351a829c6a247617dbbd5
JPeard <jinchen12@hotmail.com>
2016-02-22 16:09:18 -0700

    Added files via upload

--- src/critic_shell.cpp
+++ src/critic_shell.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "critic_shell.h"
+#include <pthread.h>
+#include <stdio.h>
 
 int c_shell(Song song) {
 	int score = 100;
@@ -21,7 +23,8 @@ int c_shell(Song song) {
 
 int supervisor(Song song) {
 	//JUDGE IT
-	int tally = MAX_NOTES;
+  int score = 100;
+  int tally = MAX_NOTES;
 
 	/* A Song holds Tracks; A Track holds Notes
 	//TODO: Finish
@@ -40,34 +43,74 @@ int supervisor(Song song) {
 	int instruments = sizeof(song.tunes)/sizeof(Track),
 		track_length = sizeof(song.tunes[0].channel)/sizeof(Note),
 		freq_ratio;
-	Note n1, n2;
+	Note a1, a2, a3, n1, n2;
 
-	// Inefficient; Could do better
+	// Parallelize on i, evaluates for errors within each track
 	for (int i = 0;i < instruments;i++) {
-		for (int j = 0;j < instruments;j++) {
-			for (int k = 0;k < track_length;k++) {
-				n1 = song.tunes[i].channel[k]; // Pick One Moment in Time
-				n2 = song.tunes[j].channel[k]; // Compare to Different Instrument
+	  for (int k = 0;k < track_length;k++) {
+	    if(score <= 0){
+	      return 0;
+	    }
+	    if(i == 0){
+	      a1 = song.tunes[i].channel[0];
+	      a2 = song.tunes[i].channel[0];
+	      a3 = song.tunes[i].channel[0];
+	    }
+	    else if(i == 1){
+	      a1 = song.tunes[i].channel[0];
+	      a2 = song.tunes[i].channel[0];
+	      a3 = song.tunes[i].channel[1];
+	    }
+	    else{
+	      a1 = song.tunes[i].channel[k-2];
+	      a2 = song.tunes[i].channel[k-1];
+	      a3 = song.tunes[i].channel[k];
+	    }
 
-				if (frequencies[n1.tone] > frequencies[n2.tone]) {
-					freq_ratio = floor((100*frequencies[n1.tone]) / frequencies[n2.tone]);
-				} else {
-					freq_ratio = floor((100*frequencies[n2.tone]) / frequencies[n1.tone]);
-				}
+	    // Not allowing two rests in a row
+	    if((a3 == 0) && (a2 == 0)){
+	      score = score - 10;
+	    }
 
-				// If there is dissonance, -1/MAX_NOTE points
-				if (!(	freq_ratio == 200 || // 2:1
-						freq_ratio == 150 || // 3:2
-						freq_ratio == 133 || // 4:3
-						freq_ratio == 100)) {	 // 1:1
-					tally--;
-					if (tally == 0) break;
-				}
-			}
-		}
+	    // Must be within an octave of the past two notes, not counting rests
+	    if(((abs(a3-a2) >= 8) && (a2 != 0)) || ((abs(a3-a1) >= 8)&& (a2 != 0)){
+	      score = score - 2;
+	    }
+	    	    
+	  }
 	}
+	  
+	    
 
-	return tally / MAX_NOTES;
+	// Parallelize on k; evaluates for errors between tracks
+	for (int k = 0;k < track_length;k++) {
+	  for (int i = 0;i < instruments;i++) {
+	    for (int j = 0;j < instruments;j++) {
+	      if(score <= 0){
+	      return 0;
+	      }
+			
+	      n1 = song.tunes[i].channel[k]; // Pick One Moment in Time
+	      n2 = song.tunes[j].channel[k]; // Compare to Different Instrument
+
+	      if (frequencies[n1.tone] > frequencies[n2.tone]) {
+		freq_ratio = floor((100*frequencies[n1.tone]) / frequencies[n2.tone]);
+	      } else {
+		freq_ratio = floor((100*frequencies[n2.tone]) / frequencies[n1.tone]);
+	      }
+
+	      // If there is dissonance, -1/MAX_NOTE points
+	      if (!(	freq_ratio == 200 || // 2:1
+			freq_ratio == 150 || // 3:2
+			freq_ratio == 133 || // 4:3
+			freq_ratio == 100))	 // 1:1
+		tally--;
+	    }
+	  }
+	}
+	score = score - tally;
+
+	return score;
 }
 
 int manual_override(Song song) {
