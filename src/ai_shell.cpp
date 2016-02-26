58078a2e75892c0cdbbaece4d6d4709c17ccf6a3
Stephen <FancifulEight@users.noreply.github.com>
2016-02-26 12:34:02 -0700

    Update ai_shell.cpp

--- src/ai_shell.cpp
+++ src/ai_shell.cpp
@@ -14,7 +14,7 @@ Song ai_shell(int score) {
 	// Init
 	if (song == null &&
 		score == 0) {
-		sprintf(buffer, 25, "python3 main.py -p %d -n %d", getpid(), rand())
+		sprintf(buffer, 25, "python3 main.py -n -p %d -s %d", getpid(), rand())
 		system(buffer);
 	}
 
@@ -29,7 +29,7 @@ Song start_AI(int score) {
 	char * buffer;
 
 	// DO YOUR STUFF
-	sprintf(buffer, 25, "python3 main.py -g", rand());
+	sprintf(buffer, 25, "python3 main.py");
 	system(buffer);
 	
 	// WAIT FOR OUTPUT (A SIGNAL FROM PYTHON'S KILL())
