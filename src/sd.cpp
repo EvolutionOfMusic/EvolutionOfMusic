a867073d59ed59ff00ef243295dc065b3ed6d6b0
Stephen <FancifulEight@users.noreply.github.com>
2016-03-02 14:51:38 -0700

    CAN WRITE TO FILE

--- src/sd.cpp
+++ src/sd.cpp
@@ -16,14 +16,41 @@ void error(const char *msg){
 
 void init_sd() {
 	ofstream toSD;
-	toSD.open("music");
-	toSD.close();
+	toSD.open("music", std::ofstream::out | std::ofstream::trunc);
+	if (toSD.is_open()) toSD.close();
 }
 
 void set_sd(vector<Song> song_list) {
 	ofstream toSD;
 	toSD.open("music");
-	for ()
-	toSD <<  <<\n";
-	toSD.close();
+	if (toSD.is_open()) {
+		// NUM_SONGS
+		toSD << song_list.size() << "\n";
+		for(std::vector<Song>::iterator it = song_list.begin();it != song_list.rend();it++) {
+			int NUM_TRACKS = sizeof((*it).tunes)/sizeof(Track);
+			//NUM_TRACKS
+			toSD << NUM_TRACKS << "\n";
+			//TEMPO
+			toSD << (*it).tempo << "\n";
+			//SONG_ID
+			toSD << (*it).song_id << "\n";
+			
+			for (int i = 0;i < NUM_TRACKS;i++) {
+				int NUM_NOTES = sizeof((*it).tunes[i])/sizeof(Note);
+				//NUM_NOTES
+				toSD << NUM_NOTES << "\n";
+				//INSTRUMENT_ID, VOLUME
+				toSD << (*it).tunes[i].instrument_id << " " << (*it).tunes[i].volume << "\n";;
+				
+				for (int j = 0;j < NUM_NOTES;j++) {
+					//PAUSE_TIME, TONE, HOLD_TIME
+					toSD 	<< (*it).tunes[i].channel[j].pause_time << " " 
+						<< (*it).tunes[i].channel[j].tone << " " 
+						<< (*it).tunes[i].channel[j].hold_time << "\n";
+				}
+			}
+		}
+		toSD << "\n";
+		toSD.close();
+	}
 }
