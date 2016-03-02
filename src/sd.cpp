4a1475655f8d0c45ec0e11d0d951b14821cd4654
Stephen <FancifulEight@users.noreply.github.com>
2016-03-02 14:20:38 -0700

    Update sd.cpp

--- src/sd.cpp
+++ src/sd.cpp
@@ -11,9 +11,19 @@
 const int port_number = 1111;
 
 void error(const char *msg){
-    perror(msg);
+	perror(msg);
+}
+
+void init_sd() {
+	ofstream toSD;
+	toSD.open("music");
+	toSD.close();
 }
 
 void set_sd(vector<Song> song_list) {
-	
+	ofstream toSD;
+	toSD.open("music");
+	for ()
+	toSD <<  <<\n";
+	toSD.close();
 }
