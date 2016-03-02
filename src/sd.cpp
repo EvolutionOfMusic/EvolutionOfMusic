729227b6f9191b5914195ab0fb92807262aafd47
Stephen <FancifulEight@users.noreply.github.com>
2016-03-02 13:40:19 -0700

    Update and rename ethernet.cpp to sd.cpp

---
+++ src/sd.cpp
@@ -0,0 +1,19 @@
+/*
+ * sd.cpp
+ *
+ *  Created on: Feb 2, 2016
+ *      Author: Stephen
+ */
+
+
+#include "sd.h"
+
+const int port_number = 1111;
+
+void error(const char *msg){
+    perror(msg);
+}
+
+void set_sd(vector<Song> song_list) {
+	
+}
