a67d3bef01dd00dbc61f8d993fc17eef16fdcece
Stephen <FancifulEight@users.noreply.github.com>
2016-03-02 13:41:48 -0700

    Update and rename ethernet.h to sd.h

---
+++ src/sd.h
@@ -0,0 +1,17 @@
+/*
+ * sd.h
+ *
+ *  Created on: Feb 1, 2016
+ *      Author: Stephen
+ */
+
+#ifndef SD_H_
+#define SD_H_
+
+#include <stdlib.h>
+#include "song_structs.h"
+
+void error(const char* msg);
+void set_sd();
+
+#endif /* SD_H_ */
