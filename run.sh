8173f8d36add97f0a5ea30089298949417f0f9fd
Stephen <FancifulEight@users.noreply.github.com>
2016-03-19 13:09:38 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,18 +5,18 @@
 echo "make"
 make -f Makefile
 
-if [ $1 = "-p" ] || [ $1 = "-c" ]; then
+if [ $1 = "-p" ] || [ $1 = "p" ] || [ $1 = "print" ] || [ $1 = "-c" ]; then
   echo "UI"
   java -jar UI.jar $1 &
   
   echo "./EvoMusic"
-#  echo "scl enable python33 bash"
-scl enable python33 "./EvoMusic $1 $2 $3"
+  #  echo "scl enable python33 bash"
+  scl enable python33 "./EvoMusic $1 $2 $3"
 else
-    echo "UI"
+  echo "UI"
   java -jar UI.jar &
   
   echo "./EvoMusic"
-#  echo "scl enable python33 bash"
-scl enable python33 "./EvoMusic $1 $2"
+  #  echo "scl enable python33 bash"
+  scl enable python33 "./EvoMusic $1 $2"
 fi
