d8d25a4ad014ed52d57448f0b02cba8f8f8f42ce
Stephen <FancifulEight@users.noreply.github.com>
2016-03-18 20:06:44 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,29 @@ scl enable python33 bash &
 echo "make"
 make -f Makefile
 
-echo "UI"
-java -jar UI.jar &
+if ["$1" = "-p"] && ["$2" = "-r"]; then
+  echo "UI"
+  java -jar UI.jar $1 $2 &
+  
+  echo "./EvoMusic"
+  ./EvoMusic $1 $3 $4
+if ["$2" = "-p"] && ["$1" = "-r"]; then
+  echo "UI"
+  java -jar UI.jar $1 $2 &
+  
+  echo "./EvoMusic"
+  ./EvoMusic $1 $3 $4
+elif ["$1" = "-p"] || ["$1" = "-r"];then
+  echo "UI"
+  java -jar UI.jar $1 &
+  
+  echo "./EvoMusic"
+  ./EvoMusic $1 $2 $3
+else
+    echo "UI"
+  java -jar UI.jar &
+  
+  echo "./EvoMusic"
+  ./EvoMusic $1 $2
+fi
 
-echo "./EvoMusic"
-./EvoMusic $1 $2
