1490f6931e13af58017a5f02d85bc6de49c829f3
Stephen <FancifulEight@users.noreply.github.com>
2016-03-19 15:23:00 -0600

    Update Overhead.cpp

--- src/Overhead.cpp
+++ src/Overhead.cpp
@@ -24,7 +24,6 @@ int main(int argc, char *argv[]) {
 	} else if (argc == 3) {
 		if (strcmp(argv[1],"c") == 0 || strcmp(argv[1],"-c") == 0 || strcmp(argv[1],"continue") == 0) {
 			continuing = true;
-			seed = 0;
 			iterations = atoi(argv[2]);
 		} else {
 			seed = atoi(argv[1]);
