b088162c1b2130c2ee313e4281ead8f02dcd253f
Stephen <FancifulEight@users.noreply.github.com>
2016-03-19 15:23:32 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ elif [ "$1" = "-c" ] || [ "$1" = "c" ] || [ "$1" = "continue" ]; then
   java -jar UI.jar -r &
   
   echo "./EvoMusic"
-  scl enable python33 "./EvoMusic $1 $2 $3"
+  scl enable python33 "./EvoMusic $1 $2"
 else
   #echo "ELSE"
   echo "UI"
