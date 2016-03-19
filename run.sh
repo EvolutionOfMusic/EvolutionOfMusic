54f40d74807361d596ffb01c3fdf83b928d3eb12
Stephen <FancifulEight@users.noreply.github.com>
2016-03-19 03:10:23 -0600

    Removed -r
    (Why did I do that in the first place?)

--- run.sh
+++ run.sh
@@ -7,30 +7,12 @@ scl enable python33 bash &
 echo "make"
 make -f Makefile
 
-if [ $1 = "-p" ] && [ $2 = "-r" ]; then
-  echo "UI"
-  java -jar UI.jar $1 $2 &
-  
-  echo "./EvoMusic"
-  ./EvoMusic $1 $3 $4
-elif [ $2 = "-p" ] && [ $1 = "-r" ]; then
-  echo "UI"
-  java -jar UI.jar $1 $2 &
-  
-  echo "./EvoMusic"
-  ./EvoMusic $2 $3 $4
-elif [ $1 = "-p" ]; then
+if [ $1 = "-p" ]; then
   echo "UI"
   java -jar UI.jar $1 &
   
   echo "./EvoMusic"
   ./EvoMusic $1 $2 $3
-elif [ $1 = "-r" ]; then
-  echo "UI"
-  java -jar UI.jar $1 &
-  
-  echo "./EvoMusic"
-  ./EvoMusic $2 $3
 else
     echo "UI"
   java -jar UI.jar &
