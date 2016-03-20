4dca68e91a54535ae140cbfd4accfec4fd624b04
Stephen <FancifulEight@users.noreply.github.com>
2016-03-20 16:37:09 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,10 @@ make -f Makefile
 
 cnt=`ps -eaflc --sort stime | grep UI.jar |grep -v grep | wc -l`
 if [ "$1" = "-p" ] || [ "$1" = "p" ] || [ "$1" = "print" ]; then
+  if [ $# -lt 3 ]; then
+    echo "Usage: $0 [-p|p|print] RandomSeed NumIterations"
+    exit
+  fi
   if(test $cnt -lt 1) ; then
     java -jar UI.jar $1 &
   fi
@@ -18,9 +22,17 @@ if [ "$1" = "-p" ] || [ "$1" = "p" ] || [ "$1" = "print" ]; then
   scl enable python33 "./EvoMusic $1 $2 $3"
   
 elif [ "$1" = "-s" ] || [ "$1" = "s" ] || [ "$1" = "supress" ]; then
+  if [ $# -lt 3 ]; then
+    echo "Usage: $0 [-s|s|supress] RandomSeed NumIterations"
+    exit
+  fi
   scl enable python33 "./EvoMusic $2 $3"
   
 elif [ "$1" = "-c" ] || [ "$1" = "c" ] || [ "$1" = "continue" ]; then
+  if [ $# -lt 2 ]; then
+    echo "Usage: $0 [-c|c|continue] NumIterations"
+    exit
+  fi
   if(test $cnt -lt 1) ; then
     java -jar UI.jar -r &
   fi
