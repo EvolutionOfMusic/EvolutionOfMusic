4d19a78c5d230fe999754f4964e6ecce3a305656
Stephen <FancifulEight@users.noreply.github.com>
2016-03-19 18:28:39 -0600

    Updated Temp Synth

--- capstone 2015 matlab and samples/music.m
+++ capstone 2015 matlab and samples/music.m
@@ -1,21 +1,56 @@
-S = 8192;
+clear
+clc
+
+S = 8192/2;
 note = @(f,d) 0.9*cos(2*pi*f*[0:1/S:d]);
 tempo = @(bbm) bbm/16;
 time = @(hold, pause, bbm) (hold - pause)/tempo(bbm);
-fs = [];
-ds = [];
-file = fopen('song_file', 'r');
+FS = [];fs = [];ds = [];
 
-file_line = fgetl(file);
-while ischar(file_line)
-    temp = sscanf(file_line, '%d %d %d');
-    fs = [fs, tone_freq(temp(2))];
-    ds = [ds, time(temp(3), temp(1), 100)];
-    file_line = fgetl(file);
-end 
-fclose(file);
+song_file = input('Enter file name: ');
 
-for index=1:numel(fs)
-    fs = [fs note(fs(index), ds(index))];
+file = fopen(song_file, 'r');
+clear song_file;
+
+num_songs = sscanf(fgetl(file), '%d');
+for i = 1:num_songs
+    num_tracks = sscanf(fgetl(file), '%d');
+    tempo = sscanf(fgetl(file), '%d');
+    song_id = sscanf(fgetl(file), '%d');
+    FS = [];
+    
+    for j = 1:num_tracks
+        fs = [];
+        ds = [];
+        num_notes = sscanf(fgetl(file), '%d'); 
+        [volume, track_id] = sscanf(fgetl(file), '%d %d');
+        clear volume track_id;
+        for k = 1:num_notes
+            temp = sscanf(fgetl(file), '%d %d %d');
+            fs = [fs, tone_freq(temp(2))];
+            ds = [ds, time(temp(3), temp(1), 40)];
+        end
+        
+        new_fs = [];
+        for index=1:length(ds)
+            new_fs = [new_fs note(fs(index), ds(index))];
+        end
+        %noteReturn = note(fs(1), ds(1));
+        [rowsFS columnsFS] = size(FS);
+        [rowsfs columnsfs] = size(fs);
+        delta = abs(columnsfs - columnsFS);
+        
+        %Preparation
+        if (columnsfs > columnsFS)
+            insert = zeros(rowsFS, columnsfs - columnsFS);
+            FS = [FS insert];
+        elseif (columnsFS > columnsfs)
+            new_fs = [new_fs zeros(1, columnsFS - columnsfs)];
+        end
+        % Polymerzation!
+        FS = [FS;new_fs];
+    end 
+    out_file = sprintf('sample_%d.wav', song_id);
+    audiowrite(out_file, FS, S) 
 end
-audiowrite('sample2.wav', fs, S)
+fclose(file) 
