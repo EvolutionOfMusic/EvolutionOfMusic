bcef469ed4f8daf9019f213615ace2fca8e2f3d8
schuman-aaron <schuman-aaron@users.noreply.github.com>
2016-03-30 21:48:58 -0600

    Knowledge is half the battle
    C2Wav outputs the waveform as expected, it is what the waveform actually sounds like is unexpected.

--- src/C2Wav/main.c
+++ src/C2Wav/main.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
 	song.song_id = 8888888;
 	song.track_num = 2;
 	song.tunes[0].track_length = 4;
-	song.tunes[0].channel[0].tone = 32;
+	song.tunes[0].channel[0].tone = 56;
 	song.tunes[0].channel[0].hold_time = 4;
 	song.tunes[0].channel[0].pause_time = 0;
 
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
 	song.tunes[0].channel[2].hold_time = 4;
 	song.tunes[0].channel[2].pause_time = 0;
 	
-	song.tunes[0].channel[3].tone = 32;
+	song.tunes[0].channel[3].tone = 56;
 	song.tunes[0].channel[3].hold_time = 4;
 	song.tunes[0].channel[3].pause_time = 0;
 	
@@ -32,7 +32,7 @@ int main(int argc, char **argv) {
 	song.tunes[1].channel[0].hold_time = 4;
 	song.tunes[1].channel[0].pause_time = 0;
 
-	song.tunes[1].channel[1].tone = 44;
+	song.tunes[1].channel[1].tone = 68;
 	song.tunes[1].channel[1].hold_time = 4;
 	song.tunes[1].channel[1].pause_time = 0;
 
@@ -40,14 +40,17 @@ int main(int argc, char **argv) {
 	song.tunes[1].channel[2].hold_time = 4;
 	song.tunes[1].channel[2].pause_time = 0;
 
-	song.tunes[1].channel[3].tone = 44;
+	song.tunes[1].channel[3].tone = 68;
 	song.tunes[1].channel[3].hold_time = 4;
 	song.tunes[1].channel[3].pause_time = 0;
 
 	song_list.push_back(song);
 
+	//printf("%i %i %i\n", song.tunes[0].channel[0].tone, song.tunes[0].channel[0].hold_time, song.tunes[0].channel[0].pause_time);
+
 	printf("Got %i song(s) from %s\n", song_list.size(), argv[1]);
-	for(std::list<Song>::iterator it = song_list.begin();
-	        it != song_list.end();it++)
-	    C2Wav((*it));
+	//for(std::list<Song>::iterator it = song_list.begin();
+	  //      it != song_list.end();it++)
+	  //  C2Wav((*it));
+	C2Wav(song);
 }
