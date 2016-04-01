095f813f8b197b3e2de03b775788cd966f72a5f7
Stephen <FancifulEight@users.noreply.github.com>
2016-03-31 21:27:19 -0600

    Update canon_in_d_test.cpp

--- src/canon_in_d_test.cpp
+++ src/canon_in_d_test.cpp
@@ -5,15 +5,17 @@
 #include <fstream>
 #include <iostream>
 
-int main() {
-  std::vector <Song> cannon;
-  int score;
+int main(int argc, char ** argv) {
+  if (argc != 2) return 1;
+  std::list <Song> cannon;
+  long long score;
   std::ifstream ifs("openingToCanonInD", std::ifstream::in);
 
   cannon = parse_song(ifs);
-  score = c_shell(cannon.at(0)); 
-
-  std::cout << "The score is: " << score << std::endl;
+  for (std::list<Song>::const_iterator iterator = cannon.begin();iterator != cannon.end();iterator++) {
+    score = c_shell(cannon.at(0)); 
+    std::cout << "The score is: " << score << std::endl;
+  }
   ifs.close();
 
   return 0;
